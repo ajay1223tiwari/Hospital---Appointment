051f3adba040b92564d88fd34af0ca547596f856
ajay tiwari <you@example.com>
2025-03-29 12:49:13 -0700

    project done

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -5,15 +5,15 @@ import { FaLinkedin, FaGithub, FaInstagram} from "react-icons/fa";
 
 const socialLinks = [
   {
-    path: "https://www.linkedin.com/in/naman-omar-a54877259/",
+    path: "https://www.linkedin.com/in/ajay-tiwari1223",
     icon: <FaLinkedin className="w-5 h-5 group-hover:text-white" />,
   },
   {
-    path: "https://www.instagram.com/naman._.omar/",
+    path: "https://www.instagram.com/ajay_tiwari6021/",
     icon: <FaInstagram className="w-5 h-5 group-hover:text-white" />,
   },
   {
-    path: "https://github.com/naman-omar",
+    path: "https://github.com/ajay1223tiwari",
     icon: <FaGithub className="w-5 h-5 group-hover:text-white" />,
   },
 ];
@@ -69,7 +69,7 @@ const Footer = () => {
           <div>
             <img src={Logo} alt="logo" />
             <p className="text-[16px] text-textColor font-[400] leading-7 mt-2">
-              Copyright &copy; {year} developed by Naman {" "}
+              Copyright &copy; {year} developed by Ajay Tiwari {" "}
               <span className="block lg:inline-block">all rights reserved</span>
             </p>
             <div className="flex items-center gap-3 mt-4">
--- README.md
+++
@@ -1,4 +0,0 @@
-# Hospital-Appointment
-Website link : "https://hospital-appointment-frontend-80rz.onrender.com/Home"
-
-A full stack project to make the hospital functional and patient-friendly
